b6411ba8e6f176549147417d28ff0d5fb7d97c9c
anamikapatidar07 <anamikapatidar201107@gmail.com>
2026-08-12 12:13:21 +0530

    Create stackpop.cpp

---
+++ stackpop.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+#include<stack>
+using namespace std;
+int main()
+{
+    stack<string> cars;
+    
+    cars.push("Volvo");
+    cars.push("BMW");
+    cars.push("Ford");
+     
+     cars.pop();
+    cout<<cars.top();
+
+    return 0;
+}
+
+//OUTPUT
+//BMW
